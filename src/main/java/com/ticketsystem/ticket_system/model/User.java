df321c4c3ecbe18366f18ad1bac40d9507b4499a
arveendYbs <arveend@ybsinternational.com>
2025-12-08 14:53:55 +0800

    test

---
+++ src/main/java/com/ticketsystem/ticket_system/model/User.java
@@ -0,0 +1,16 @@
+package com.ticketsystem.ticket_system.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+}
